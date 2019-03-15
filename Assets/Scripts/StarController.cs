85c13a155ce587d1fdd03a981671c7415e75a4af
白川泰平 <ganth36@gmail.com>
2019-03-15 16:36:27 +0900

    PinBall Lesson5 7.2まで
    オブジェクトが光らない。

---
+++ Assets/Scripts/StarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour
+{
+    float speed = 1.0f;
+ //   public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.transform.Rotate(0,Random.Range(0,360),0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, this.speed, 0);
+    }
+}
